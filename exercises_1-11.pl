3d0f3c0517e5ae9bb5e2e9eb7816edd8b2fea50f
Kidel <tano.design@gmail.com>
2016-04-13 21:52:14 +0200

    all excercises done

--- exercises_1-11.pl
+++ exercises_1-11.pl
@@ -1,3 +1,24 @@
+%% genealogico
+
+genitore(tommaso,francesca).
+genitore(tommaso,vittorio).
+genitore(francesca,linda).
+genitore(francesca,leonardo).
+genitore(vittorio,bianca).
+genitore(vittorio,andrea).
+genitore(bianca,tommaso).
+genitore(gianroberto,vittorio). 
+
+nonno(X,Y) :- genitore(X,Z), genitore(Z,Y).
+
+bisnonno(X,Y) :- genitore(X,Z), nonno(Z,Y).
+fratello(X,Y) :- genitore(Z,X), genitore(Z,Y), X\=Y. 
+zio(X,Y) :- genitore(Z,Y), fratello(Z,X).
+nipote(X,Y) :- nonno(Y,X); zio(Y,X).
+bisnipote(X,Y) :- bisnonno(Y,X). 
+discendente(X,Y) :- genitore(Y,X); nonno(Y,X); bisnonno(Y,X); (genitore(Y,Z), discendente(X,Z)).
+cugino(X,Y) :- genitore(A,X), genitore(B,Y), fratello(A,B).
+
 %% fact(+X,?Y), vero se Y è il fattoriale di X.
 
 fact(0, 1) :- !.
@@ -6,8 +27,8 @@ fact(X, Y) :-  X>0, Z is (X-1), fact(Z, K), Y is (K*X), !.
 
 %% palindroma(X)
 
-palindroma([]) :- !.
-palindroma([_]) :- !.
+palindroma([]).
+palindroma([_]).
 palindroma([X|Rest]) :- append(Y,[X],Rest), palindroma(Y), !.
 
 
@@ -76,14 +97,15 @@ rev([X|Rest], Y) :- append(Tser, [X], Y), rev(Rest, Tser), !.
 
 %% del_first(+X,+L,?Resto)  (fails if X is not a member)
 
-del_first(_,[],_) :- fail. %% not really necessary
+del_first(_,[],_) :- fail. %% not necessary
 del_first(X,[X|Rest],Rest) :- !. 
-del_first(X,[Y|Rest],Clean) :- del_first(X,Rest,CleanNoY), append([Y],CleanNoY, Clean). %% X different from Y, because of the previous cut
+del_first(X,[Y|Rest],Clean) :-  %% implicit that X different from Y, because of the previous cut
+		del_first(X,Rest,CleanNoY), append([Y],CleanNoY, Clean). 
 
 
 %% del(+X,+L,?Resto)
 
-del(_,[],[]) :- !. %% not really necessary
+del(_,[],[]) :- !. %% not necessary
 del(X,L,L) :- \+ member(X, L), !. 
 del(X,Y,Clean) :- del_first(X,Y,PartialClean), del(X,PartialClean,Clean).
 
@@ -104,22 +126,130 @@ mkset([_|Rest],Set) :- mkset(Rest,Set).
 
 %% union(+A,+B,-Union)
 
-union(A,B,Union) :- append(A, B, Append), mkset(Append, Union).
+union(A,B,Union) :- append(A,B,Append), mkset(Append,Union).
 
 
 %% occurs in(?X,+Y)
 
 occurs_in(_, []) :- !, fail. 
 occurs_in(X, X). 
-occurs_in(X, [Y|Rest]) :- occurs_in(X, Y); occurs_in(X, Rest).
+occurs_in(X, [Y|Rest]) :- occurs_in(X, Y); occurs_in(X,Rest), X\=Rest.
 
 
 %% flat(+X,?Y)
 
 flat([], []) :- !.
 flat(X, [X]) :- \+ is_list(X), !.
-flat([X|Rest], Flat) :- flat(X, FlatOne), flat(Rest, FlatTwo), append(FlatOne, FlatTwo, Flat).
+flat([X|Rest],Flat) :- flat(X,FlatOne), flat(Rest,FlatTwo), append(FlatOne,FlatTwo,Flat).
 
 
+%% cartprod(+A,+B,-Set)
+
+prod_first(_,[],[]).
+prod_first(X,[Y|Coda],PartialSet) :- PartialSet = [[X,Y]|Rest], prod_first(X, Coda, Rest).
+
+cartprod([],_,[]) :- !.
+cartprod(_,[],[]) :- !.
+cartprod([A|Tail],B,Set) :- 
+			prod_first(A, B, NewSetA), 
+			cartprod(Tail,B,NewSetTail),
+			append(NewSetA,NewSetTail,Set), !.
+			
+cartprod2(A,B,Set) :- findall([X,Y], (member(X,A), member(Y,B)), Set).
+
+
+%% insert(X,L1,L2)
+
+insert(X,L,[X|L]).
+insert(X,[Y|L1],[Y|L2]) :- insert(X,L1,L2).
+
+
+%% permut(X,Y)
+
+permut([], []).
+permut([X|Tail],Permut) :- permut(Tail, Part),  insert(X, Part, Permut).
+
+
+%% search subset(+IntList,+N,?Set)
+
+search_subset([],0,[]).
+search_subset([X|Tail1],N,[X|Tail2]) :- M is N-X, search_subset(Tail1,M,Tail2).
+search_subset([_|Tail1],N,Set) :- search_subset(Tail1,N, Set).
+
+
+%% trees
+
+bin_height(empty,0).
+bin_height(t(_,Left,Right),H) :- bin_height(Left,N), bin_height(Right,M), (N>M, H is N+1 ; N=<M, H is M+1).
+
+bin_size(empty,0).
+bin_size(t(_,Left,Right),H) :- bin_size(Left,N), bin_size(Right,M), H is N+M+1.
+
+reflect(empty,empty).
+reflect(t(X,empty,empty),t(X,empty,empty)).
+reflect(t(X,Left,Right),t(X,Right,Left)).
+
+bin_labels(empty,[]).
+bin_labels(t(X,empty,empty),[X]).
+bin_labels(t(X,Left,Right),[X|Rest]) :- 
+		bin_labels(Left,L),
+		bin_labels(Right,R),
+		append(L,R,Rest).
+		
+balanced(empty).
+balanced(t(_,Left, Right)) :- 
+		bin_height(Left, LeftH), 
+		bin_height(Right, RightH), 
+		X is abs(RightH-LeftH), 
+		X=<1, 
+		balanced(Left), 
+		balanced(Right).
+		
+branch(t(Leaf,empty, empty),Leaf,[Leaf]).
+branch(t(X,Left,_), Leaf, [X|Tail]) :- branch(Left, Leaf, Tail).
+branch(t(X,_,Right), Leaf, [X|Tail]) :- branch(Right, Leaf, Tail).
+
+%% Root => Subtrees
+
+op(600,xfx,=>).
+
+height(empty, 0).
+height(_ => SubTree,N) :- !, height(SubTree,X), N is X+1.
+height([], 0) :- !.
+height([X|SubTrees],N) :- !, height(X,This), height(SubTrees,Partial), max(This,Partial,N).
+height(_, 1).
+
+size(empty, 0).
+size(_ => SubTree,N) :- !, size(SubTree, X), N is X+1.
+size([], 0) :- !.
+size([X|SubTrees],N) :- !, size(X,This), size(SubTrees,Other), N is This+Other.
+size(_, 1).
+
+label(empty, []).
+label(Root => SubTree,[Root|Tail]) :- !, label(SubTree, Tail). 
+label([], []) :- !.
+label([X|SubTrees],Labels) :- !, label(X,LabelX), label(SubTrees,OtherLabels), append(LabelX,OtherLabels,Labels).
+label(X, [X]).
+
+
+%% path 
+
+arc(a,b).
+arc(a,e).
+arc(b,a).
+arc(b,c).
+arc(c,c).
+arc(c,d).
+arc(d,c).
+arc(d,b).
+arc(e,c).
+
+path(Start, Goal, Path) :- path(Start, Goal, Path, []).
+
+path(Start, Start, [Start], Visited) :- \+ member(Start, Visited).
+path(Start, Goal, [Start|Path], Visited) :-
+		\+ member(Start, Visited), 
+		arc(Start, N), 
+		path(N, Goal, Path, [Start|Visited]).
 
 
