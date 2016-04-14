533f35f1c6577bf3722ffb7f850b0e5552d420f1
Kidel <tano.design@gmail.com>
2016-04-14 11:41:15 +0200

    added exam solutions

--- exercises_random.pl
+++ exercises_random.pl
@@ -17,3 +17,17 @@ merge(A,[],A).
 merge([],B,B).
 merge([A|Ra],[B|Rb],[A|M]) :-  A =< B, merge(Ra,[B|Rb],M).
 merge([A|Ra],[B|Rb],[B|M]) :-  A > B,  merge([A|Ra],Rb,M).
+
+
+%% delete_leaf(?Leaf, +Tree, ?NewTree)
+
+delete_leaf(X,t(X,empty,empty),empty).
+delete_leaf(X,t(Y,Left1,Right),t(Y,Left2,Right)) :- delete_leaf(X,Left1,Left2).
+delete_leaf(X,t(Y,Left,Right1),t(Y,Left,Right2)) :- delete_leaf(X,Right1,Right2).
+
+
+%% replace_node(+Node, ?Replace, +Tree, ?NewTree)
+
+replace_node(A, B, t(A,C,D), t(B,C,D)).
+replace_node(A, B, t(E, C1, D), t(E, C2, D)) :- replace_node(A, B, C1, C2).
+replace_node(A, B, t(E, C, D1), t(E, C, D2)) :- replace_node(A, B, D1, D2).
